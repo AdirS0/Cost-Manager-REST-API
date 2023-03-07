e8975e91e3e7f5fed012e4553dac282f3d8be63a
Adir Solomon <79449128+AdirS0@users.noreply.github.com>
2023-03-07 17:15:23 +0200

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,13 +1,3 @@
-/* Developers Details: 
-  First Name:Hai
-  Last Name:Elimelech
-  Id:315942458
-
-  First Name:Adir
-  Last Name:Solomon
-  Id:206825440
-  */
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
