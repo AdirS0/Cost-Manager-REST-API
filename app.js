989c24323e70352919f4acec458ba756ea21f992
Adir Solomon <adir199@gmail.com>
2023-07-29 15:30:33 +0300

    add controllers and improve code

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on('error',(error) => console.error(error));
-db.once('open',() => console.log('Connected to Database'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
+// Routes:
+const addCostRouter = require("./routes/addcost");
+app.use("/addcost", addCostRouter);
 
-//Routes:
-const addCostRouter = require('./routes/addcost');
-app.use('/addcost',addCostRouter);
+const reportRouter = require("./routes/report");
+app.use("/report", reportRouter);
 
-const reportRouter = require('./routes/report');
-app.use('/report',reportRouter);
+const aboutRouter = require("./routes/about");
+app.use("/about", aboutRouter);
 
-const aboutRouter = require('./routes/about');
-app.use('/about',aboutRouter);
-
-
-app.listen(3000, () => {console.log('Web service listening on port 3000');
+app.listen(3000, () => {
+  console.log("Web service listening on port 3000");
 });
