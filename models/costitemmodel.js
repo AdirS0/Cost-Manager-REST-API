6dc54515fd29557b20f027b5de62723c52f2fd51
Adir Solomon <adir199@gmail.com>
2023-07-29 15:32:46 +0300

    improve code

--- models/costitemmodel.js
+++ models/costitemmodel.js
@@ -1,17 +1,24 @@
-  const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-  const costItemSchema = new mongoose.Schema({
-      user_id: String,
-      year: Number,
-      month: Number,
-      day: Number,
-      description: String,
-      category: {
-        type: String,
-        enum: ['food', 'health', 'housing', 'sport',
-         'education', 'transportation', 'other']
-      },
-      sum: Number
-    });
-    
-  module.exports = mongoose.model('costs', costItemSchema);
+const costItemSchema = new mongoose.Schema({
+  user_id: String,
+  year: Number,
+  month: Number,
+  day: Number,
+  description: String,
+  category: {
+    type: String,
+    enum: [
+      "food",
+      "health",
+      "housing",
+      "sport",
+      "education",
+      "transportation",
+      "other",
+    ],
+  },
+  sum: Number,
+});
+
+module.exports = mongoose.model("costs", costItemSchema);
