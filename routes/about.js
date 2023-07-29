1b7da191aed7c2c6ce74151307a58b4a43a8d30e
Adir Solomon <adir199@gmail.com>
2023-07-29 15:35:50 +0300

    add aboutController

--- routes/about.js
+++ routes/about.js
@@ -1,31 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const CostItemModel = require('../models/costitemmodel');
+const aboutController = require("../controllers/aboutController");
 
-router.get('/', (req, res) => {
-    return new Promise((resolve, reject) => {
-      const developers = [
-        {
-          firstname: "Hai",
-          lastname: "Elimelech",
-          id: "00000",
-          email: "hai842@gmail.com"
-        },
-        {
-         firstname: "Adir",
-          lastname: "Solomon",
-          id: "11111",
-          email: "adir199@gmail.com"
-        }
-      ];
-      resolve(developers);
-    })
-    .then((developers) => {
-      res.send(developers);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-  });
+router.get("/", aboutController.getDevelopersInfo);
 
-  module.exports = router;
+module.exports = router;
