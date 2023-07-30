7867574f00be488afe236f483a79e1efd364b3cb
Adir Solomon <adir199@gmail.com>
2023-07-30 17:07:39 +0300

    add report controller object

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,73 +1,50 @@
 const CostItemModel = require("../models/costitemmodel");
 const ReportModel = require("../models/reportmodel");
 
-async function getReport(req, res) {
-  const categories = [
-    "food",
-    "health",
-    "housing",
-    "sport",
-    "education",
-    "transportation",
-    "other",
-  ];
+const reportController = {
+  async getReport(req, res) {
+    const { user_id, month, year } = req.query;
 
-  const { user_id, month, year } = req.query;
-
-  const checkReport = await ReportModel.findOne({
-    user_id: user_id,
-    month: month,
-    year: year,
-  });
-
-  if (!checkReport) {
-    console.log("Report does not exist");
-    const costItemsByQuery = await CostItemModel.find({
-      user_id,
-      month,
-      year,
-    });
-
-    const report = new ReportModel({
+    const checkReport = await ReportModel.findOne({
       user_id: user_id,
-      year: year,
       month: month,
+      year: year,
     });
 
-    if (costItemsByQuery.length === 0) {
-      categories.forEach((category) => {
-        report[category] = [];
+    if (!checkReport) {
+      const report = new ReportModel({
+        user_id: user_id,
+        year: year,
+        month: month,
       });
+
+      console.log(`Report for user id: ${user_id} does not exist, creating new one`);
+      const costItemsByQuery = await CostItemModel.find({
+        user_id,
+        month,
+        year,
+      });
+
+      if (costItemsByQuery.length === 0) {
+        report.total_sum = 0;
+      } else {
+        let total_sum = 0;
+        costItemsByQuery.forEach(({ id, day, description, sum, category }) => {
+          report[category].push({ id, day, description, sum });
+          total_sum += sum;
+        });
+        report.total_sum = total_sum;
+      }
+
+      report
+        .save()
+        .then(() => res.status(201).json(report))
+        .catch((error) => res.status(500).send(error));
     } else {
-      costItemsByQuery.forEach(({ id, day, description, sum, category }) => {
-        if (!report[category]) {
-          report[category] = [];
-        }
-        report[category].push({ id, day, description, sum });
-      });
+      console.log(`Report for user id: ${user_id} exists`);
+      res.status(200).json(checkReport);
     }
-    report
-      .save()
-      .then(() => {
-        ReportModel.findOne({
-          user_id: user_id,
-          month: month,
-          year: year,
-        })
-          .then((report) => res.status(201).json(report))
-          .catch((error) => res.status(500).send(error));
-      })
-      .catch((error) => res.status(500).send(error));
+  },
+};
 
-    /*
-      report.save()
-      .then(() => res.send('report added successfully!'))
-      .catch((error) => res.send(error));
-      */
-  } else {
-    console.log("Report exists");
-    res.status(201).json(checkReport);
-  }
-}
-
-module.exports = { getReport };
+module.exports = reportController;
