15381133a0b9ba6475e36e4f29356ce72f9d80b4
Adir Solomon <adir199@gmail.com>
2023-07-30 17:07:13 +0300

    add cost controller object

--- controllers/costController.js
+++ controllers/costController.js
@@ -1,21 +1,50 @@
 const CostItemModel = require("../models/costitemmodel");
+const ReportModel = require("../models/reportmodel");
 
-function addCostItem(req, res) {
-  console.log(req.body);
-  const costItem = new CostItemModel({
-    user_id: req.body.user_id,
-    year: req.body.year,
-    month: req.body.month,
-    day: req.body.day,
-    description: req.body.description,
-    category: req.body.category,
-    sum: req.body.sum,
-  });
+const costController = {
+  async addCostItem(req, res) {
+    const costItem = new CostItemModel({
+      user_id: req.body.user_id,
+      year: req.body.year,
+      month: req.body.month,
+      day: req.body.day,
+      description: req.body.description,
+      category: req.body.category,
+      sum: req.body.sum,
+    });
 
-  costItem
-    .save()
-    .then(() => res.send("Cost item added successfully!"))
-    .catch((error) => res.send(error));
-}
+    let id = "";
+    await costItem
+      .save()
+      .then((savedCostItem) => {
+        id = savedCostItem._id;
+        console.log(id);
+      })
+      .catch((error) => res.error(error));
+      console.log(id);
 
-module.exports = { addCostItem };
+    let existingReport = await ReportModel.findOne({
+      user_id: req.body.user_id,
+      year: req.body.year,
+      month: req.body.month,
+    });
+
+    if (existingReport) {
+      const { day, description, sum, category } = req.body;
+      
+      existingReport[category].push({ id, day, description, sum });
+      existingReport
+        .save()
+        .then(() =>
+          res
+            .status(201)
+            .send("Cost item added successfully, Existing Report updated")
+        )
+        .catch((error) => res.status(500).send(error));
+    } else {
+      res.status(201).send("Cost item added successfully, no Existing Report");
+    }
+  },
+};
+
+module.exports = costController;
